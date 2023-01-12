2e304244358d1143df8659bd632877417de27cbc
Mariusmivw <mvan-wij@student.codam.nl>
2023-01-12 17:07:22 +0100

    use exception and move utils

--- class/ParsedRequest.cpp
+++ class/ParsedRequest.cpp
@@ -2,6 +2,32 @@
 #include <algorithm>
 #include <cctype>
 
+namespace util {
+	static std::vector<std::string> split_string(std::string str, std::string delim) {
+		std::vector<std::string> result;
+
+		size_t pos = 0;
+		while (true) {
+			size_t found = str.find(delim, pos);
+
+			if (found == std::string::npos) {
+				result.push_back(str.substr(pos));
+				return result;
+			}
+			result.push_back(str.substr(pos, found - pos));
+			pos = found + delim.length();
+		}
+	}
+
+	static unsigned char char_to_lower(char c) {
+		return std::tolower(c);
+	}
+
+	static void str_to_lower(std::string &str) {
+		std::transform(str.begin(), str.end(), str.begin(), &char_to_lower);
+	}
+}
+
 std::ostream &operator<<(std::ostream &os, const Method &method) {
 	static std::map<Method, std::string> methods;
 	methods[GET] = "GET";
@@ -18,35 +44,21 @@ std::ostream &operator<<(std::ostream &os, const Method &method) {
 	return os;
 }
 
-static std::vector<std::string> split_string(std::string str, std::string delim) {
-	std::vector<std::string> result;
-	size_t pos = 0;
-	while (true) {
-		size_t found = str.find(delim, pos);
-
-		if (found == std::string::npos) {
-			result.push_back(str.substr(pos));
-			return result;
-		}
-		result.push_back(str.substr(pos, found - pos));
-		pos = found + delim.length();
-	}
-}
-
 ParsedRequest::ParsedRequest(std::string str) {
+	std::string metadata;
 	size_t found = str.find("\r\n\r\n");
 	if (found == std::string::npos) {
 		this->body = "";
+		metadata = str;
 	}
 	else {
 		this->body = str.substr(found + 4);
+		metadata = str.substr(0, found);
 	}
-	std::string metadata = str.substr(0, found);
-	std::vector<std::string> lines = split_string(metadata, "\r\n");
-	std::vector<std::string> first_line_parts = split_string(lines[0], " ");
+	std::vector<std::string> lines = util::split_string(metadata, "\r\n");
+	std::vector<std::string> first_line_parts = util::split_string(lines[0], " ");
 	if (first_line_parts.size() != 3) {
-		std::cout << "Invalid first line" << std::endl;
-		exit(1); // TODO: throw exception
+		throw std::runtime_error("Invalid first line");
 	}
 	this->method = ParsedRequest::parse_method(first_line_parts[0]);
 	this->path = first_line_parts[1];
@@ -66,16 +78,12 @@ Method ParsedRequest::parse_method(std::string method_str) {
 	methods["OPTIONS"] = OPTIONS;
 	methods["TRACE"] = TRACE;
 	methods["PATCH"] = PATCH;
-	if (methods.count(method_str) != 0)
-		return methods[method_str];
-	return UNKNOWN;
+	if (methods.count(method_str) == 0)
+		throw std::runtime_error("Invalid method");
+	return methods[method_str];
 }
 
-static unsigned char low(char c) {
-	return std::tolower(c);
-}
-
-std::map<std::string, std::string>	ParsedRequest::parse_headers(std::vector<std::string> const lines) {
+std::map<std::string, std::string> ParsedRequest::parse_headers(std::vector<std::string> const lines) {
 	std::map<std::string, std::string> headers;
 	std::string delimiter(": ");
 	size_t delim_length = delimiter.length();
@@ -83,7 +91,7 @@ std::map<std::string, std::string>	ParsedRequest::parse_headers(std::vector<std:
 		std::string line = *it;
 		size_t delim_pos = line.find(delimiter);
 		std::string key = line.substr(0, delim_pos);
-		std::transform(key.begin(), key.end(), key.begin(), &low );
+		util::str_to_lower(key);
 		std::string value = line.substr(delim_pos + delim_length);
 		headers[key] = value;
 	}

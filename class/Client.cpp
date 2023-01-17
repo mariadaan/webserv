cb3a56622efe57d6d274ade4feed0163e9fdf519
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2023-01-17 16:41:19 +0100

    add default constructor

--- class/Client.cpp
+++ class/Client.cpp
@@ -3,8 +3,7 @@
 
 Client::Client(int client_sockfd, sockaddr_in client_address, socklen_t client_address_len)
 	: _client_sockfd(client_sockfd), _client_address(client_address), _client_address_len(client_address_len)
-{
-}
+{}
 
 // TODO: should we also remove it from the map in the Socket class?
 void Client::close(void) {
--- class/Client.hpp
+++ class/Client.hpp
@@ -9,6 +9,7 @@
 class Client
 {
 public:
+	Client() {}
 	Client(int client_sockfd, sockaddr_in client_address, socklen_t client_address_len);
 	int			get_sockfd(void) const;
 	void		close(void);

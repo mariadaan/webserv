aa63c712249c7348d3b2874db374764e4e3fdae3
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2023-03-02 17:53:37 +0100

    cleaning

--- src/server/Response.cpp
+++ src/server/Response.cpp
@@ -229,8 +229,6 @@ void Response::_send_error_response() {
 	this->_client.send(header_end);
 
 	this->_client.send(page_content);
-
-	logger << Logger::debug << "Sent error response" << std::endl;
 }
 
 void Response::_send_status(HTTP_STATUS_CODES status_code) {
@@ -249,7 +247,7 @@ bool Response::send() {
 	}
 	this->_ready = SENT;
 	if (this->has_error_status()) {
-		logger << Logger::info << "Sending error response" << std::endl;
+		logger << Logger::info << "Sending error response "<< this->_status_code << std::endl;
 		this->_send_error_response();
 		return (true);
 	}

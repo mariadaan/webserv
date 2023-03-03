663e6e3187fbca780dc076f71c9ed25492d5ce5a
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2023-03-03 17:37:11 +0100

    show error page when no name is provided

--- root/usr/lib/cgi-bin/get_name.py
+++ root/usr/lib/cgi-bin/get_name.py
@@ -5,19 +5,22 @@ import cgi
 form = cgi.FieldStorage()
 
 # Get data from fields
-rev_first_name = form.getvalue('first_name')[::-1].lower().capitalize()
-rev_last_name  = form.getvalue('last_name')[::-1].lower().capitalize()
-
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
 
 print('HTTP/1.1 200 OK')
 print('Content-Type: text/html\r\n\r\n')
-
 print('<html>')
 print('<head>')
 print('<title>Reverse name</title>')
 print('</head>')
 print('<body>')
 print('<h1>Reverse name</h1>')
-print('<h2>Your name in reverse is \"%s %s\"</h2>' % (rev_first_name, rev_last_name))
+if not first_name or not last_name:
+	print('<h2>Please enter a first and last name!</h2>')
+else:
+	rev_first_name = first_name[::-1].lower().capitalize()
+	rev_last_name = last_name[::-1].lower().capitalize()
+	print('<h2>Your name in reverse is \"%s %s\"</h2>' % (rev_first_name, rev_last_name))
 print('</body>')
 print('</html>')
